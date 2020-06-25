9562624d9931e3b9219389984e1d60476e34366d
Sean Scamehorn <smscamehorn45@gmail.com>
2020-06-25 02:50:40 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,17 +8,21 @@ def using_sort(array)
 end
 
 def using_reverse(array)
-
+ array = ["wow", "I", "am", "really", "learning", "arrays!"]
+ array.reverse
 end
 
 def using_first(array)
-
+ array = ["wow", "I", "am", "really", "learning", "arrays!"]
+ array.first
 end
 
 def using_last(array)
-
+ array = ["wow", "I", "am", "really", "learning", "arrays!"]
+ array.last
 end
 
 def using_size(array)
-
+ array = ["wow", "I", "am", "really", "learning", "arrays!"]
+ array.size
 end
